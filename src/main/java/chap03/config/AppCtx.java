e4f6e1135629120b7747198ed52ebe0491d5b4fb
EomHyeYeong <hy30051@naver.com>
2024-04-17 16:19:58 +0900

    ch3 스프링을 이용한 객체 조립과 사용

---
+++ src/main/java/chap03/config/AppCtx.java
@@ -0,0 +1,30 @@
+package chap03.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import chap03.spring.ChangePasswordService;
+import chap03.spring.MemberDao;
+import chap03.spring.MemberRegisterService;
+
+@Configuration
+public class AppCtx {
+	
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao();
+	}
+	
+	@Bean
+	public MemberRegisterService memberRegSvc() {
+		return new MemberRegisterService(memberDao());
+	}
+	
+	@Bean
+	public ChangePasswordService changePwdSvc() {
+		ChangePasswordService pwdSvc = new ChangePasswordService();
+		pwdSvc.setMemberDao(memberDao());
+		return pwdSvc;
+	}
+
+}
